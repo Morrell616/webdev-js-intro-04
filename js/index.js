8aa645855332ca2e891d22b60958d80f4c13bc99
Jen Morrell <morrell616@hotmail.com>
2024-12-28 19:31:33 -0500

    added practice if/else statements and commented out code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,60 @@
 // Prevent us from attempting to use variables
 // that are not declared
+/*
 "use strict"
+let healthPoints = 10;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+let jenIsAGirl = true;
+
+if (jenIsAGirl === true) {
+    console.log ("lady");
+}
+
+let dinnerPlate = false;
+
+if (dinnerPlate === true) {
+    console.log ("Delicious");
+}
+
+let fruit = true;
+if (fruit === true) {
+    console.log ("Use in a smoothie");
+} else {
+    console.log ("do not use in somoothie");
+}
+let age = 35;
+ 
+if (age >= 21) {
+    console.log("Congratulations you can vote and buy alcohol");
+} else if (age < 21 && age >= 18) {
+    console.log("You are able to vote!");
+} else {
+    console.log("you have some growing to do");
+}
+
+let number = 5;
+
+if (number >= 18) {
+    console.log ("Adult");
+} else {console.log ("Minor")
+
+}
+
+let grade = 60;
+
+if (grade >= 80) {
+    console.log ("honor roll");
+} else if (grade < 80 && grade >= 65) {
+    console.log ("Average");
+}else {
+    console.log ("Needs Improvement")
+}
+*/
